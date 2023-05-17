bef533bdb99606c99ddbe792d542e41829765ad0
FreeSaladEggs <meychlibi@gmail.com>
2023-05-17 13:39:15 +0100

    Spawn manager (kinda fixed)

--- QuiverProject/Assets/Scripts/SpawnManager.cs
+++ QuiverProject/Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject Enemy;
-    private float spawnRangex = 12;
-    private float spawnPosZ = 0;
-    public float startDelay = 2;
-    public float spawnInterval = 1.5f;
+    private float spawnRangex = 100;
+    private float spawnRangexm = 35;
+    private float spawnPosZm = 90;
+    private float spawnPosZ = 50;
+    private float spawnPosY = 21.92f;
+    public float startDelay = 5;
+    public float spawnInterval = 9;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +24,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnEnemies()
     {
 
-        Vector3 SpawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), 21.143f, spawnPosZ);
+        Vector3 SpawnPos = new Vector3(Random.Range(spawnRangexm, spawnRangex), spawnPosY, Random.Range(spawnPosZ,spawnPosZm) );
 
         Instantiate(Enemy, SpawnPos, Enemy.transform.rotation);
 
