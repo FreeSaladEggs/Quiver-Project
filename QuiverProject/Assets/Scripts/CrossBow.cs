fe3b08f88a56a98d2a57f861eea15d5576369c47
Therealkhalil <khalil.chabir96@gmail.com>
2023-05-19 05:33:26 +0100

    Fix Arrow Bug + Add mechanics Crossbow
    Add mechanics Crossbow with shooting arrow inside only

--- QuiverProject/Assets/Scripts/CrossBow.cs
+++ QuiverProject/Assets/Scripts/CrossBow.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class CrossBow : MonoBehaviour
 {
     public GameObject arrow;
+    public bool ready_shoot; // ready to shoot
+   // public CrossBowAttack crossbow;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Arrow"))
         {
             Debug.Log("arrow on crossbow");
-            //attack
+            ready_shoot = true;
         }
     }
 }
---
+++ QuiverProject/Assets/Scripts/CrossBowAttack.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class CrossBowAttack : MonoBehaviour
+{
+    public CrossBow CheckArrow; // access to script if arrow is in crossbow
+    Rigidbody rb;
+    bool shooted_already = false; // already shooted
+    public GameObject Placement; // placement to push arrow
+    public GameObject arrowWorld; // arrow real world
+    public GameObject arrowInst; // arrow prefab
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (CheckArrow.ready_shoot && GetTriggerInput())
+         {
+             Shoot();
+             Debug.Log("Test");
+         }
+
+         if(shooted_already)
+         {
+             shooted_already = false;
+             CheckArrow.ready_shoot = false;
+             Instantiate(arrowInst,Placement.transform.position,Quaternion.identity);
+             Destroy(arrowWorld);
+         }
+    }
+
+    public bool GetTriggerInput()
+    {
+        // Check if the left mouse button is pressed
+        return Mouse.current.leftButton.isPressed;
+    }
+
+    public void Shoot()
+    {
+        rb.isKinematic = false;
+        shooted_already = true;
+    }
+}
